b8bf940bbab14d2a6f30adb79934d9c17a434275
Vishal Naik <vishalmanohar@gmail.com>
2014-08-31 13:12:51 -0400

    feature to promote new result to baseline

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var fs = require('fs')
 
 var app = express();
 var rootScreenshotFolder = '/Users/vishal/tw_projects/banner/cinnamon/rwd-tests/build/results/'
+var baselineScreenshotFolder = '/Users/vishal/tw_projects/banner/cinnamon/rwd-tests/site/screenshots/'
 
 app.set('port', (process.env.PORT || 5000))
+app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(rootScreenshotFolder + "tablet"))
 app.use(express.static(rootScreenshotFolder + "desktop"))
@@ -21,7 +23,10 @@ app.get('/files/:device', function(request, response) {
   	var imageFiles = files.filter(function (file) {return file.substr(-4) === ".png"})
     var groupedFileNames = []
 
-  	var originalFiles = imageFiles.filter(function (file) {return (file.indexOf(".fail.") == -1  &&  file.indexOf(".diff.")  == -1) })
+  	var originalFiles = imageFiles.filter(function (file) {
+      return (file.indexOf(".fail.") == -1  &&  file.indexOf(".diff.")  == -1); 
+    });
+
   	originalFiles.forEach(function(file){
 		var displayName = file.replace(".png", "")//.replace("tablet_", "").replace("mobile_", "").replace("desktop_", "")
   		var errorFileName = file.replace(".png", ".fail.png");
@@ -39,4 +44,14 @@ app.get('/files/:device', function(request, response) {
   	 }
   	 response.send(data)
   });
+});
+
+app.post('/promote', function(request, response) {
+  var device = request.body.device;
+  var fileNameToPromote = request.body.file;
+  var baselineFilename = fileNameToPromote.replace(".diff", "");
+  fs.createReadStream(rootScreenshotFolder + device + "/" + fileNameToPromote)
+    .pipe(fs.createWriteStream(baselineScreenshotFolder + device + "/" + baselineFilename));
+    
+  response.send("ok");
 });
